78bfb970e9216b5bad2a9e312655cba8e778d1ad
Mushfiq Rashid <mushfiqarashid@gmail.com>
2025-09-22 19:09:49 -0700

    add js code for creating random code

---
+++ js/randomcodes.js
@@ -0,0 +1,24 @@
+// Function to generate combination of characters
+function generateCode() {
+    // Create variables to store generated codes and the type of characters we want to show as codes
+    var code = ' '; // initialize to null value
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@$';
+
+    // Generate character multiple times using a loop
+    for (i = 1; i <= 8; i++) {
+        var char = Math.random() * str.length; // randomly generate a number based on the length of the string
+        code += str.charAt(char); // acculumate the generated char into a string of 8 characters
+    }
+    return code; // return the final accumulated generated code
+}
+
+// Get the html element to display
+document.getElementById("codes").innerHTML = generateCode();
+
+// disable button
+function disableButton() {
+    document.getElementById("submitBtn").disabled = true;
+}
+
+// active func
+disableButton();
