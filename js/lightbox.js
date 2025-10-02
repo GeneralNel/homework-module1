fca9b62b1d8e3360a07b603b43922a64af213183
Mushfiq Rashid <mushfiqarashid@gmail.com>
2025-10-01 19:04:31 -0700

    add the lightbox logic using jquery

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,2 +1,26 @@
 $(document).ready(function(){
+
+    /* Open lightbox on button click */
+    $('.cliff-image').click(function(){
+        $('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
+        $('.box').fadeIn();
+
+        //Check if lightbox has an image
+        if ($('.box').contents('img')) {
+            $('.box').contents().remove('img'); //If true, clear image
+        }
+
+        //Clone the clicked image and add it to the lightbox
+        var img = $(this).clone(); //Duplicate DOM element
+        $('.box').append(img); //Insert duplicated element in another element
+    });
+
+    /* Click to close lightbox */
+    $('.close, .backdrop').click(function(){
+        $('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
+            $('.backdrop').css('display', 'none');
+        });
+        $('.box').fadeOut();
+    });
+
 });
