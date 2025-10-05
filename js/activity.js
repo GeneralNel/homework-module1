73fdee622950fa604a10624f30695f5f3398605a
Mushfiq Rashid <mushfiqarashid@gmail.com>
2025-10-04 17:26:58 -0700

    add styling on selected and hovered cells

--- js/activity.js
+++ js/activity.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
         var content = $(this).text(); //get content of cell
 
         if (content != "Not Available") { //check if content does not contain a particular string
-            $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected
+            $(this).toggleClass("tdHighlight"); //add or remove class when cell is selected
 
-            if ($(this).hasClass("tdhighlight")) { //check if selected cell has class
+            if ($(this).hasClass("tdHighlight")) { //check if selected cell has class
                 $('#displaySelected').css("visibility", "visible"); //make display box visible
                 $('#displaySelected').css("margin-top", "2em"); //add spaces above display box
                 $('#result').append("<p>" + content + "</p>"); //add child element with contents of cell
@@ -18,6 +18,13 @@ $(document).ready(function () {
                     $('#displaySelected').css("margin-top", "0"); //remove spaces above display box
                 }
             }
+        }
+    });
+    $("td").hover(function () {
+        var content = $(this).text(); //get content of cell
+
+        if (content != "Not Available") { //check if content does not contain a particular string
+            $(this).css("cursor", "pointer");
         }
     });
 });
