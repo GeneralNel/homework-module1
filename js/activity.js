eaf09da81954ec981aaadaf9baba79ed3b641b48
Mushfiq Rashid <mushfiqarashid@gmail.com>
2025-10-04 17:32:33 -0700

    add location next to activity in the output box

--- js/activity.js
+++ js/activity.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
     $("td").click(function () { //user select a table data cell
         var content = $(this).text(); //get content of cell
+        var col = $(this).index(); //get index of selected cell
 
-        if (content != "Not Available") { //check if content does not contain a particular string
+        if (content != "Not Available" && col != 0) { //check if content does not contain a particular string
             $(this).toggleClass("tdHighlight"); //add or remove class when cell is selected
 
             if ($(this).hasClass("tdHighlight")) { //check if selected cell has class
+                var header = $("th").eq(col).text(); //get content of header cell based on index of selected cell
+                content = content + " at " + header; //add header content to selected cell content
+
                 $('#displaySelected').css("visibility", "visible"); //make display box visible
                 $('#displaySelected').css("margin-top", "2em"); //add spaces above display box
                 $('#result').append("<p>" + content + "</p>"); //add child element with contents of cell
@@ -22,8 +26,9 @@ $(document).ready(function () {
     });
     $("td").hover(function () {
         var content = $(this).text(); //get content of cell
+        var col = $(this).index(); //get index of selected cell
 
-        if (content != "Not Available") { //check if content does not contain a particular string
+        if (content != "Not Available" && col != 0) { //check if content does not contain a particular string
             $(this).css("cursor", "pointer");
         }
     });
